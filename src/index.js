50651f4bdb02bb1afdcc9996a2eb14ddb1fdf75f
Daniel Curran <dcurran@fullfacing.com>
2019-10-18 13:55:15 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const dhtLib = require('node-dht-sensor');
+const {
+  Thing,
+  Value,
+  Property,
+} = require('webthing');
+
+const config = {
+  id: 'urn:dev:ops:dht-22-sensor',
+  name: 'DHT-22',
+  description: 'Ambient temperature & humidity sensor',
+  interval: 3000,
+  sensor: {
+    type: 22,
+    pin: 4,
+  },
+  properties: {
+    temperature: {
+      '@type': 'TemperatureProperty',
+      title: 'Temperature',
+      type: 'number',
+      description: 'The ambient temperature measured in °C',
+      unit: 'degree celcius',
+      readOnly: true,
+    },
+    humidity: {
+      '@type': 'HumidityProperty',
+      title: 'Humidity',
+      type: 'number',
+      description: 'The current relative humidity in %',
+      minimum: 0,
+      maximum: 100,
+      unit: 'percent',
+      readOnly: true,
+    }
+  }
+}
+
+class WebThingsDHT {
+  constructor(opt = config) {
+    this.opt = opt
+  }
+  init() {
+    const { id, name, description, properties, interval } = this.opt
+    const dht = new Thing(id, name, ['Temperature', 'Humidity'], description);
+    const dht_temperature = new Value(0.0);
+    const dht_humidity = new Value(0.0);
+    dht.addProperty(new Property(dht, 'temperature', dht_temperature, properties.temperature));
+    dht.addProperty(new Property(dht, 'humidity', dht_humidity, properties.humidity));
+    setInterval(() => {
+      try {
+        const { temperature, humidity } = this.read()
+        // Update the underlying value, which in turn notifies all listeners
+        dht_temperature.notifyOfExternalUpdate(temperature);
+        dht_humidity.notifyOfExternalUpdate(humidity);
+      } catch (e) {
+        console.log(e)
+      }
+    }, interval);
+    return dht
+  }
+  read() {
+    const { type, pin } = this.opt.sensor
+    return dhtLib.read(type, pin)
+  }
+}
+
+module.exports = WebThingsDHT
